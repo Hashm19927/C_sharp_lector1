ac5a30ac14bf962457c3077a6f428e532a70e757
horrorjoy <hashm19927@gmail.com>
2023-02-09 00:23:44 +0100

    добавил комментарии в описание синтакиса

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -2,3 +2,6 @@
 string username = Console.ReadLine();
 Console.WriteLine("Привет, ");
 Console.WriteLine(username);
+//Write() - Вывод в одну строку
+//WriteLine() - В конце перейти на новую строку
+//ReadLine() - Считать строку из терминала

846e3fafdfc99aece9598c58b17ad57606d63a4c
horrorjoy <hashm19927@gmail.com>
2023-02-12 11:51:44 +0100

    methodarray

--- Example010_Array/Program.cs
+++ Example010_Array/Program.cs
@@ -16,17 +16,3 @@ int result=Max(
     Max(array[6],array[7],array[8]));
 
 Console.WriteLine(result);
-/*int[] array ={1,12,31,4,15,16,17,18};
-
-int n = array.Length;
-int find=4;
-
-int index=0;
-
-while (index<n)
-{
-    if (array[index] ==find)
-    {
-        Console.WriteLine(index);
-    }
-}*/
---
+++ Example011_methodInArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array={1,12,13,14,15,16,17,18,19};
+
+int n =array.Length;
+int find =4;
+
+int index=0;
+
+while (index<n)
+{
+    if (array[index]==find)
+    {
+        Console.WriteLine(index);
+    }
+    index++;
+}
